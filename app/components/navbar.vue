928f9df7f691c2e2b75116bb2ddc05a40700a8d2
Enthusiastic-yash <yashkashyap1005@gmail.com>
2025-10-29 15:44:33 +0530

    completed home landing page

---
+++ app/components/navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-primary shadow-sm">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="pl-6 text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign In
+        <icon name="tabler:brand-github" size="24" />
+      </a>
+    </div>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto mt-5 min-h-96">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travel and adventures with this simple travel log app. Add locations, photos, and
+          notes
+          to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with Github
+          <icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
