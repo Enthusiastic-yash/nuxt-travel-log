26210777e8463e4213568cf8d94adf6d089fbb96
yash kashyap <82098201+Enthusiastic-yash@users.noreply.github.com>
2026-03-10 00:32:43 +0530

    created sidebar (#59)

---
+++ app/components/sidebar-button.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+const route = useRoute();
+</script>
+
+<template>
+  <div
+    class=" tooltip-right"
+    :data-tip="props.label"
+    :class="{ tooltip: !showLabel }"
+  >
+    <NuxtLink
+      :class="{ 'bg-base-200': route.path === props.href, 'justify-center': !props.showLabel, 'pl-2': props.showLabel }"
+      :to="props.href"
+      class="flex py-1 gap-x-2 flex-nowrap"
+    >
+      <Icon
+        :name="props.icon"
+        size="24"
+      />
+      <Transition name="grow">
+        <span v-if="props.showLabel">
+          {{ props.label }}
+        </span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.3s ease-in-out;
+}
+.grow-leave-active {
+  animation: grow 0.3s ease-in-out reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
+const isSideBarOpen = ref(false);
 
+function toggleSideBar() {
+  isSideBarOpen.value = !isSideBarOpen.value;
+  localStorage.setItem("isSideBarOpen", isSideBarOpen.value.toString());
+}
+
+onMounted(() => {
+  isSideBarOpen.value = localStorage.getItem("isSideBarOpen") === "true";
+});
 </script>
 
 <template>
-  <div>
-    <h1>You are now logged In</h1>
+  <div class="flex flex-1">
+    <div class="bg-base-100 transition-all ease-in-out duration-400" :class="{ 'w-64': isSideBarOpen, 'w-16': !isSideBarOpen }">
+      <div class="flex" :class="{ 'justify-end': isSideBarOpen, 'justify-center': !isSideBarOpen }">
+        <Icon
+          :name="isSideBarOpen ? 'tabler:chevron-left' : 'tabler:chevron-right'"
+          size="32"
+          class="cursor-pointer p-2"
+          @click="toggleSideBar"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <SidebarButton
+          icon="tabler:map"
+          label="Locations"
+          href="/dashboard"
+          :show-label="isSideBarOpen"
+        />
+        <SidebarButton
+          icon="tabler:circle-plus-filled"
+          label="Add Location"
+          href="/dashboard/add"
+          :show-label="isSideBarOpen"
+        />
+
+        <div class="divider" />
+        <SidebarButton
+          icon="tabler:logout-2"
+          label=" Sign Out"
+          href="/sign-out"
+          :show-label="isSideBarOpen"
+        />
+      </div>
+    </div>
+
+    <div class="flex-1 bg-amber-200" />
   </div>
 </template>
 
