ad08c2b65d8a5ac81e6f54fe82957630e9fd2673
yash kashyap <82098201+Enthusiastic-yash@users.noreply.github.com>
2026-04-14 17:26:49 +0530

    deleted location (#73)

--- app/pages/dashboard/location/[slug].vue
+++ app/pages/dashboard/location/[slug].vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import type { FetchError } from "ofetch";
+
 const route = useRoute();
 const locationStore = useLocationStore();
 const { currentLocation: location, currentLocationStatus: status, currentLocationError: error } = storeToRefs(locationStore);
 const mapStore = useMapStore();
+
+const isOpen = ref(false);
+const deleteError = ref("");
+const isDeleting = ref(false);
 
 watchEffect(() => {
   if (location.value) {
@@ -16,6 +22,31 @@ watchEffect(() => {
   }
 });
 
+function openDialog() {
+  isOpen.value = true;
+  (document.activeElement as HTMLAnchorElement).blur();
+}
+
+async function confirmDelete() {
+  try {
+    isOpen.value = false;
+    deleteError.value = "";
+    isDeleting.value = true;
+    await $fetch(`/api/location/${route.params.slug}`, {
+      method: "DELETE",
+    });
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    deleteError.value = error.statusMessage;
+  }
+  isDeleting.value = false;
+}
+
+const loading = computed(() => status.value === "pending" || isDeleting.value);
+const errorMessage = computed(() => error.value?.statusMessage || deleteError.value);
+
 // onBeforeRouteUpdate((to) => {
 //   if (to.name === "dashboard-location-slug") {
 //     locationStore.refreshCurrentLocation();
@@ -25,13 +56,40 @@ watchEffect(() => {
 
 <template>
   <div class="p-4 min-h-64 container max-w-md">
-    <div v-if="status === 'pending'">
+    <div v-if="errorMessage && !loading">
+      <h2 class="text-lg alert alert-error">
+        {{ errorMessage }}
+      </h2>
+    </div>
+    <div v-if="loading">
       <div class="loading " />
     </div>
-    <div v-if="route.name === 'dashboard-location-slug' && location && status !== 'pending'">
+    <div v-if="route.name === 'dashboard-location-slug' && location && !loading">
       <h2 class="text-xl">
         {{ location?.name }}
+        <div class="dropdown dropdown-bottom">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn m-1 btn-sm p-o"
+          >
+            <Icon name="tabler:dots-vertical" size="20" />
+          </div>
+          <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <NuxtLink @click="openDialog()">
+                <Icon name="tabler:trash-x-filled" size="20" />
+                Delete
+              </NuxtLink>
+              <NuxtLink :to="{ name: 'dashboard-location-slug-edit', params: { slug: route.params.slug } }">
+                <Icon name="tabler:map-pin-cog" size="20" />
+                Edit
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </h2>
+
       <p class="text-sm">
         {{ location?.description }}
       </p>
@@ -45,15 +103,19 @@ watchEffect(() => {
         </button>
       </div>
     </div>
-    <div v-if="error && status !== 'pending'">
-      <h2 class="text-lg alert alert-error">
-        {{ error.statusMessage }}
-      </h2>
-    </div>
 
     <div v-if="route.name !== 'dashboard-location-slug'">
       <NuxtPage />
     </div>
+    <Dialog
+      title="Are you sure ? "
+      description="Deleting this location will also delete all of the associated logs. This cannot be undone Do you really want to do this ?"
+      :is-open="isOpen"
+      confirm-label="Yes, Delete this location"
+      confirm-class="btn-error"
+      @on-closed="isOpen = false"
+      @on-confirmed="confirmDelete"
+    />
   </div>
 </template>
 
